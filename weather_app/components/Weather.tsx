55fdbd24cf960e3bc9a6e2b9e1151dc93d6c9629
ARJUN P <arjunpchr@gmail.com>
2024-08-16 14:30:42 +0530

    test: finishing stage

--- weather_app/components/Weather.tsx
+++ weather_app/components/Weather.tsx
@@ -33,7 +33,7 @@ const Weather = () => {
     setLoading(true);
     try {
       const geocodeResponse = await axios.get(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`
+        `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`
       );
       const { lat, lon } = geocodeResponse.data[0];
       const response = await axios.get(
